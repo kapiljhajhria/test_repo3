72e4bb74624b652ed37f779c85e9c96e9602672c
kapil jhajhria <jhajhria44@gmail.com>
2019-09-26 21:09:38 +0530

    Stateless widget use, by using stateless widget, if we just update the code inside stateless widget, it will hot reload just that widget and we will be able to see the output of our code a lot faster.

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("My first Flutter App"),
-            centerTitle: true,
-            backgroundColor: Colors.red[600],
-          ),
-          body: Center(
-            child: Text(
-              'Body of my first app',
-              style: TextStyle(
-                fontSize: 24,
-                decoration: TextDecoration.underline,
-                fontWeight: FontWeight.w900,
-                fontFamily: 'Mansalva',
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Text('tap'),
-            backgroundColor: Colors.red[600],
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first Flutter App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Body of my first app',
+          style: TextStyle(
+            fontSize: 24,
+            decoration: TextDecoration.underline,
+            fontWeight: FontWeight.w900,
+            fontFamily: 'Mansalva',
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('tap'),
+        backgroundColor: Colors.red[600],
+      ),
     );
+  }
+}
